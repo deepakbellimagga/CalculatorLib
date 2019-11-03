6496885c75ba2e469a8960c5ef2c8e785aa28414
deepakbellimagga <57325036+deepakbellimagga@users.noreply.github.com>
2019-11-03 13:22:13 -0800

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -7,7 +7,7 @@ class TestCalculator:
         assert 4 == calculator.add(2, 2)
 
     def test_additionone(self):
-        assert 4 == calculator.add(2, 3)
+        assert 5 == calculator.add(2, 3)
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
